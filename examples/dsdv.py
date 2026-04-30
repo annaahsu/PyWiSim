69d10c775601c1a5db8c90bb17c243d7ea67293b
Anna Hsu <adhsu@usc.edu>
2026-04-30 15:54:09 -0700

    dsdv v3

--- examples/dsdv.py
+++ examples/dsdv.py
@@ -16,49 +16,52 @@ class DSDVNode(Node):
         }
 
     def on_receive(self, msg, sender):
-        _, orig, dest, seq, metric, hops = msg
+        # _, orig, dest, seq, metric, hops = msg
+        _, source, seq, metric = msg
 
-        if self.routing_table.get(dest) == None:
-            self.routing_table[dest] = {
+        if source == self.nid:
+            return
+
+        existing = self.routing_table.get(source)
+
+        if existing == None:
+            self.routing_table[source] = {
                 "next_hop": sender,
                 "metric": metric + 1,
                 "seq_num": seq
             }
-            self.broadcast(('', orig, dest, seq, metric + 1, hops))
+            self.broadcast(('', source, seq, metric + 1))
         else:
-            existing = self.routing_table[dest]
             if seq > existing['seq_num']:
-                self.routing_table[dest] = {
+                self.routing_table[source] = {
                     "next_hop": sender,
                     "metric": metric + 1,
                     "seq_num": seq
                 }
 
-                self.broadcast(('', orig, dest, seq, metric + 1, hops))
+                self.broadcast(('', source, seq, metric + 1))
             elif seq == existing['seq_num']:
                 if metric + 1 < existing['metric']:
-                    self.routing_table[dest]["next_hop"] = sender
-                    self.routing_table[dest]["metric"] = metric + 1
+                    self.routing_table[source]["next_hop"] = sender
+                    self.routing_table[source]["metric"] = metric + 1
 
-                    self.broadcast(('', orig, dest, seq, metric + 1, hops))
+                    self.broadcast(('', source, seq, metric + 1))
 
-    def periodic_broadcast(self, dest):
-        self.seq += 1
-        self.neighbors.add((self.nid, dest, self.seq, self.seq))
-        self.net.log(f"{self.nid}: route discovery -> {dest}")
-        self.broadcast(('', self.nid, dest, 0, self.seq, 0))
+    def periodic_broadcast(self):
+        self.seq += 2
+        # self.net.log(f"{self.nid}: route discovery -> {dest}")
+        self.broadcast(('', self.nid, self.seq, 0))
 
 # --- network: line topology A--B--C--D--E ---
 loop = EventLoop()
-net = WirelessNetwork(loop, tx_range=1.5, tx_time=0.8, seed=7, verbose=True)
+net = WirelessNetwork(loop, tx_range=1, tx_time=0.8, seed=7, verbose=True)
 for nid, x, y in [('A',0,0), ('B',1,0), ('C',2,0), ('D',3,0), ('E',4,0)]:
     net.add_node(DSDVNode(nid), x, y)
 
 print("Topology:", {n: net.neighbors(n) for n in net.nodes})
 
 for i in net.nodes:
-    for j in net.nodes:
-        loop.schedule(1.0, net.nodes[i].periodic_broadcast, j)
+    loop.schedule(1.0, net.nodes[i].periodic_broadcast)
 loop.run(until=25)
 
 
